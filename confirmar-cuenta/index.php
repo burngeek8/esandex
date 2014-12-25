020fe65c7a6b9706005966a89080be63853bbc81
burngeek8 <burngeek8@gmail.com>
2014-12-25 00:20:05 -0500

    cambiando a enlaces relativos confirmar cuenta

--- confirmar-cuenta/index.php
+++ confirmar-cuenta/index.php
@@ -7,18 +7,18 @@
 	<meta name="HandheldFriendly" content="true">
 	<!-- Hojas de estilo -->
 	<link rel="shortcut icon" type="image/x-icon" href="../img/logoIco.png">
-	<link rel="stylesheet" type="text/css" href="/css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="/css/main.css">	
+	<link rel="stylesheet" type="text/css" href="../css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">	
 	<!-- Scripts -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="/js/main.js"></script>
+	<script type="text/javascript" src="../js/main.js"></script>
 	
 </head>
 <body>
 	<div class="formulario">
 		<h1>Confirmar Cuenta</h1>
 		<div id="respuestaConfirmacionCuenta" class="cajaDialogo">
-			<img src="/img/loader.gif">
+			<img src="../img/loader.gif">
 		</div>
 		<form id="formConfirmarCuenta" method="post">
 			<label>Email</label>
@@ -32,7 +32,7 @@
 	</div>
 	<div class="opcionCuenta">
 		<p>¿No tienes una cuenta?</p>
-		<a href="/register">Crear una cuenta</a>
+		<a href="../register">Crear una cuenta</a>
 	</div>
 </body>
 </html>
