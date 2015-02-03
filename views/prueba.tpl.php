7d2100a82e2ec84fed8feeb97e2bd6e87bba22b8
burngeek8 <burngeek8@gmail.com>
2015-02-02 20:34:55 -0500

    esandex home dev

---
+++ views/prueba.tpl.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Esandex</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="HandheldFriendly" content="true" />
+	<!-- Hojas de estilo -->
+	<link rel="shortcut icon" type="image/x-icon" href="img/ico.png" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<!-- Scripts -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+	<header>
+		<div class="logo"></div>
+		<nav>
+			<ul>
+				<li>
+					<a href="login">Login</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<section class="contenido">
+		<section class="dominios">
+			<h2>Dominios</h2>
+			<form>
+				<input type="text" placeholder="Dominio a consultar (www.esandex.com)" />
+				<input type="email" placeholder="Email" />
+				<input type="submit" value="Consultar">
+			</form>
+		</section>
+		<section class="contactanos">
+			
+		</section>
+	</section>
+	<footer>
+		<p>Esandex 2015</p>
+	</footer>
+</body>
+</html>
